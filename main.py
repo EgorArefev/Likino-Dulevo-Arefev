87a680c9022c85ee35f44d826c8c1a5c5fb2c235
Egor <arefevegor086@gmail.com>
2022-11-10 17:28:48 +0300

    Likino-Dulevo-Arefev

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def main():
-    print('My first git program')
-    print('And I change it every day')
-
-
-if __name__ == '__main__':
+print('My first git-repo')
+print('My first git-repo')
+print('My first git-repo')
+print('My first git-repo')
+print('My first git-repo')
